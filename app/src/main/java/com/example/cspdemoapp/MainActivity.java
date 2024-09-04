12e1950d11478ebd0ade5c2f56c1c238a67387e8
HEMANTHKUMARANNAM <hemanthkumar312004@gmail.com>
2024-09-04 22:12:27 +0530

    ADD

---
+++ app/src/main/java/com/example/cspdemoapp/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.cspdemoapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button admin,attendance;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        admin = findViewById(R.id.button);
+        attendance= findViewById(R.id.button2);
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        admin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(MainActivity.this,ADMINACTIVITY.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        attendance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(MainActivity.this,ATTENDANCEACTIVITY.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
+
+
+
+
+    }
+}
